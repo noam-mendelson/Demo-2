8e11d1fb76769a8e6e077e9b66fffe8ec1296c5d
noam-mendelson <noammendelson115@gmail.com>
2023-09-19 07:48:48 +1000

    loading images from keras folder

--- UNet.py
+++ UNet.py
@@ -1,6 +1,18 @@
-    
-    # Initialize a Sequential model
-    
+from PIL import Image
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+
+def load_images_from_folder(folder_path):
+    images = []
+    for filename in sorted(os.listdir(folder_path)):
+        if filename.endswith(".png"):
+            img_path = os.path.join(folder_path, filename)
+            img = Image.open(img_path).convert('L')  # Convert image to grayscale
+            img = np.array(img)
+            images.append(img)
+    return np.array(images)
+
     # Encoder (Downsampling)
     # Add a series of Conv2D, Activation, and MaxPooling layers
     # can also add BatchNormalization and Dropout layers 
